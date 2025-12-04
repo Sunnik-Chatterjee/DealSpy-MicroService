b5756e50940e5f6af22ae28a0c7ec0197544121f
Sunnik <sunnikchatterjee21@gmail.com>
2025-12-04 23:16:01 +0530

    FEAT: Added prefix

--- app/api/router.py
+++ app/api/router.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api.update_router import router as update_router
 from app.api.search_router import router as search_router 
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/micro")  # Add prefix here
 api_router.include_router(update_router)
 api_router.include_router(search_router) 
